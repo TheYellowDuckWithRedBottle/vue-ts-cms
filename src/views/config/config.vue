e23abbb71d3ebdc3d0b9c3bacc6bc81a04db799d
fanyutan <fanyutan@gtmap.cn>
2022-10-12 19:16:41 +0800

    feat: 完成部分配置

---
+++ src/views/config/config.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="config-panel">
+    <div class="config-header">
+      <div class="config-title-container">
+        <span class="config-title">管理配置</span>
+        <span class="version">v1.0</span>
+      </div>
+      <div
+        class="use-help"
+        @mousemove="hoverHelp(true)"
+        @mouseleave="hoverHelp(false)"
+        :class="isShowHelpDoc ? 'hover-help' : ''"
+      >
+        <div class="use-tip-container">
+          <span class="use-tip">使用帮助</span>
+          <font-awesome-icon icon="fa-solid fa-circle-question" />
+        </div>
+        <div class="use-detail" v-show="isShowHelpDoc">
+          <div>PDF文档</div>
+          <div>word文档</div>
+        </div>
+      </div>
+    </div>
+    <div class="config-body">
+      <div class="config-navMenu">
+        <configNavMenu />
+      </div>
+      <div class="menu-body"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import configNavMenu from './configBody/configNavMenu.vue'
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  components: {
+    configNavMenu
+  },
+  setup() {
+    let isShowHelpDoc = ref(false)
+    function hoverHelp(isHover: boolean) {
+      isShowHelpDoc.value = isHover
+    }
+    return {
+      hoverHelp,
+      isShowHelpDoc
+    }
+  }
+})
+</script>
+
+<style scoped>
+.config-panel {
+  width: 100%;
+  height: 100%;
+}
+.config-header {
+  box-sizing: border-box;
+  width: 100%;
+  height: 45px;
+  background: rgb(45, 109, 204);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 16px;
+  color: white;
+}
+.config-title {
+  font-size: 18px;
+  font-weight: bold;
+}
+.version {
+  font-size: 12px;
+}
+.use-help {
+  font-size: 16px;
+  padding: 0 4px;
+  height: 100%;
+  position: relative;
+}
+.use-help:hover {
+  background: rgba(255, 255, 255, 0.2);
+  cursor: pointer;
+}
+.help-title {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+.use-tip-container {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+.use-tip {
+  margin-right: 4px;
+}
+.use-detail {
+  width: 100%;
+  height: 200%;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-content: center;
+  /* position: absolute; */
+}
+.use-detail:hover {
+  background: rgba(255, 255, 255, 0.2);
+  cursor: pointer;
+}
+.use-tip,
+.config-title {
+  padding-right: 8px;
+}
+.config-body {
+  height: calc(100% - 45px);
+  width: 100%;
+  background: #efeef0;
+}
+.config-navMenu {
+  width: 200px;
+  height: 100%;
+  border-right: 1px solid #e5e5e5;
+}
+</style>
---
+++ src/views/config/configBody/configNavMenu.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="panel">
+    <template v-for="(item, index) in configMenu" :key="index">
+      <div class="config-menu-item">
+        <div class="config-menu-title">
+          <div>
+            <font-awesome-icon icon="fa-solid fa-bars" />
+            <span class="config-title-icon">{{ item.title }}</span>
+          </div>
+          <div>
+            <font-awesome-icon icon="fa-solid fa-bars" />
+          </div>
+        </div>
+        <template v-if="item.children && item.children.length > 0">
+          <div
+            v-for="(child, childIndex) in item.children"
+            :key="childIndex"
+            class="config-child"
+          >
+            <div>{{ child.title }}</div>
+          </div>
+        </template>
+      </div>
+    </template>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  setup() {
+    const configMenu = [
+      { title: '模板管理', icon: '' },
+      {
+        title: '视频管理',
+        children: [{ title: '视频监控' }, { title: '视频统计' }],
+        icon: ''
+      },
+      { title: '系统管理', icon: '' }
+    ]
+    return { configMenu }
+  }
+})
+</script>
+
+<style scoped>
+.panel {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: grey;
+  padding: 10px 0;
+  font-size: 12px;
+}
+.config-menu-item {
+  width: 100%;
+}
+.config-menu-title {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  padding: 8px;
+  border-bottom: 1px solid #e5e5e5;
+}
+.config-child {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+}
+.config-title-icon {
+  padding-left: 4px;
+}
+</style>
