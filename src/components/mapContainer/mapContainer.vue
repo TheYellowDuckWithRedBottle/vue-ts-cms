abb5ec10ba9f74128521a2dcef65c2379106bc6a
fanyutan <fanyutan@gtmap.cn>
2023-02-18 23:28:03 +0800

    feat(import): 添加坐标定义和属性表展示

--- src/components/mapContainer/mapContainer.vue
+++ src/components/mapContainer/mapContainer.vue
@@ -29,7 +29,7 @@ export default defineComponent({
       const instance = getCurrentInstance()
       positionControl()
       var map = L.map('map', {
-        crs: L.CRS.EPSG3857,
+        crs: CRS_4490,
         attributionControl: false,
         zoomControl: false
       }).setView([32, 120.84], 16)
