7227ecb28d6db44e7d5907a9424359ac34590193
Levi_Fan <1521681359@qq.com>
2024-01-06 16:51:32 +0800

    chore(package): 去除eslint依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
+    '@vue/typescript/recommended'
   ],
   parserOptions: {
     ecmaVersion: 2020
