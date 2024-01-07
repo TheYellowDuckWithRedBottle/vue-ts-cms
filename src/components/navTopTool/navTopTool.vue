63719431692a04859b2a148fcb1c573cddaf6459
Levi_Fan <1521681359@qq.com>
2024-01-07 16:34:08 +0800

    fix(navTopTool): 添加坐标黏贴组件

--- src/components/navTopTool/navTopTool.vue
+++ src/components/navTopTool/navTopTool.vue
@@ -2,6 +2,13 @@
   <div class="top-tool">
     <div class="collapse-wrap" @click="collapseMenuPanel">
       <font-awesome-icon icon="fa-solid fa-bars" />
+      <!-- 靠右 -->
+    </div>
+    <div class="copy-coordinate">
+      <font-awesome-icon icon="fa-solid fa-copy" />
+        <div class="copy-panel">
+
+        </div>
     </div>
   </div>
 </template>
@@ -27,7 +34,7 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .top-tool {
   position: absolute;
   z-index: 2;
@@ -38,10 +45,28 @@ export default defineComponent({
   display: flex;
   align-items: center;
   padding: 0 8px;
+
 }
+    .copy-coordinate {
+    height: 100%;
+    width: calc(100% - 25px);
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    color: #428bca;
+    font-weight: 600;
+    position:relative;
+
+  }
+      .copy-panel {
+      position: absolute;
+      width: 500px;
+      height: 300px;
+    }
 .collapse-wrap {
   color: #428bca;
   font-size: 20px;
+  width: 25px;
 }
 .collapse-wrap:hover {
   cursor: pointer;
